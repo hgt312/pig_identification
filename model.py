96b0d640bdb5cfde476984f024ac6f787bcb2485
hgt <11610130@mail.sustc.edu.cn>
2017-11-22 14:11:57 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,24 @@
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.layers import Dense
 from keras.models import Model
+from keras.callbacks import TensorBoard
 from keras.preprocessing.image import ImageDataGenerator
 
 from utils import get_nb_files
 
+
+# 准备数据
+train_dir = '/media/hgt/share2/jdd/train'  # 训练集数据
+dev_dir = '/media/hgt/share2/jdd/dev'  # 验证集数据
+
+# 改这里就好了
+nb_classes = 30  # 分类数
+nb_train_samples = get_nb_files(train_dir)      # 训练样本个数
+nb_val_samples = get_nb_files(dev_dir)       # 验证集样本个数
+batch_size = 128
+epochs = 50
+steps_per_epoch = int(nb_train_samples / batch_size)
+optimizer = 'adam'
 
 # cnn部分，使用了inception resnet v2
 # pooling参数: 'max'/'avg'/None
@@ -22,15 +36,7 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # 编译模型及选择优化方法
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-
-# 准备数据
-train_dir = '/media/hgt/share2/jdd/train'  # 训练集数据
-dev_dir = '/media/hgt/share2/jdd/dev'  # 验证集数据
-
-nb_classes = 30  # 分类数
-nb_train_samples = get_nb_files(train_dir)      # 训练样本个数
-nb_val_samples = get_nb_files(dev_dir)       # 验证集样本个数
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
 train_datagen = ImageDataGenerator(
     rescale=1. / 255,
@@ -42,8 +48,8 @@ dev_datagen = ImageDataGenerator(rescale=1. / 255)
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
-    target_size=(299, 299),  # all images will be resized to 150x150
-    batch_size=32,
+    target_size=(299, 299),
+    batch_size=batch_size,
     class_mode='categorical')
 
 dev_generator = dev_datagen.flow_from_directory(
@@ -55,9 +61,10 @@ dev_generator = dev_datagen.flow_from_directory(
 
 # 训练模型
 model.fit_generator(
-        train_generator,
-        steps_per_epoch=645,
-        epochs=1,
-        validation_data=dev_generator,
-        validation_steps=9000)
+    train_generator,
+    steps_per_epoch=steps_per_epoch,
+    epochs=epochs,
+    validation_data=dev_generator,
+    validation_steps=276,
+    callbacks=[TensorBoard(log_dir='./log')])
 model.save_weights('/media/hgt/share2/jdd/try_time_1st.h5')
