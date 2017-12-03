8cde59044d031010931959f932a46c4881129a4e
hgt <963542954@qq.com>
2017-12-03 11:56:34 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -32,14 +32,14 @@ def separate():
     else:
         os.mkdir(os.path.join(root_path, 'train'))
     for i in range(1, 31):
-        if os.path.exists(os.path.join(root_path, 'dev', str(i))):
+        if os.path.exists(os.path.join(root_path, 'dev', 'pig' + str(i))):
             pass
         else:
-            os.mkdir(os.path.join(root_path, 'dev', str(i)))
-        if os.path.exists(os.path.join(root_path, 'train', str(i))):
+            os.mkdir(os.path.join(root_path, 'dev', 'pig' + str(i)))
+        if os.path.exists(os.path.join(root_path, 'train', 'pig' + str(i))):
             pass
         else:
-            os.mkdir(os.path.join(root_path, 'train', str(i)))
+            os.mkdir(os.path.join(root_path, 'train', 'pig' + str(i)))
     for i in range(1, 31):
         now_path = os.path.join(origin_path, str(i))
         name_list = list(os.path.join(now_path, name) for name in os.listdir(now_path))
@@ -47,17 +47,36 @@ def separate():
         print(name_list[:3])
         for num in range(len(name_list)):
             if num < int(len(name_list) * 0.7):
-                shutil.copy(name_list[num], os.path.join(train_dir, str(i)))
+                shutil.copy(name_list[num], os.path.join(train_dir, 'pig' + str(i)))
             else:
-                shutil.copy(name_list[num], os.path.join(dev_dir, str(i)))
+                shutil.copy(name_list[num], os.path.join(dev_dir, 'pig' + str(i)))
+
+
+def as_num(x):
+    y = '{:.9f}'.format(x)
+    return y
+
+
+def check_csv(file='./results/loss1.csv'):
+    with open(file, mode='r') as f:
+        count = 0
+        for line in f:
+            count += float(line.split(',')[2])
+        print(count)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--separate", action="store_true")
+    parser.add_argument("-c", "--check")
+    parser.add_argument("-p", "--predict")
     args = parser.parse_args()
     if args.separate:
         separate()
+    if args.check:
+        check_csv(args.check)
+    if args.predict:
+        check_csv(args.predict)
 
 
 if __name__ == '__main__':
